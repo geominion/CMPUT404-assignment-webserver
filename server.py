14415d0771340d52a3b4da81198dc453ca00782c
geominion <joshua@ualberta.ca>
2014-01-17 20:11:29 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import SocketServer
 # coding: utf-8
+import os
+from os import curdir
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,17 +30,61 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
+
     
     def handle(self):
+
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        hold = self.data
+        "".join(hold)
+        parsed = hold.split()
+        verify = False
+        response = ""
+        path = parsed[1]
+        initial = "HTTP/1.1 ";
+        conType  = "Content-Type: text/"
+        webdir = "/www"
+        default = "/index.html"
+        check = "." + path
+        for dirname, dirnames, filenames in os.walk("./www"):
+            dirname = dirname.replace("/www", "")
+            for subdirname in dirnames:
+                dirpath = os.path.join(dirname, subdirname)
+                if check == dirpath:
+                    path = curdir + webdir + path
+                    verify = True
+                elif check == (dirpath + "/"):
+                    path = curdir + webdir + "/" + path + default
+                    verify = True
+            for filename in filenames:
+                if check == os.path.join(dirname, filename):
+                    path = curdir + webdir + path
+                    verify = True
+        type = "css"
+        if path.endswith(type) != True:
+            type = "html"
+        if parsed[0] == "GET":
+            if path == "/":
+                verify = True
+                path = curdir + webdir + default
+            if verify == True:
+                f = open(path)
+                page = f.read()
+                f.close()
+                response = initial + "200 OK\n" + conType + type + "\n\n\n" + page
+            else:
+                response = initial + "404 NOT FOUND\n" + conType + type + "\n\n" + "<h1>404 NOT FOUND</h1>"
+        else:
+            response = initial + "404 NOT FOUND\n" + conType + type + "\n\n" + "<h1>404 NOT FOUND</h1>"
+        self.request.sendall(response)
+                
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
     SocketServer.TCPServer.allow_reuse_address = True
-    # Create the server, binding to localhost on port 9999
+    # Create the server, binding to localhost on port 8080
     server = SocketServer.TCPServer((HOST, PORT), MyWebServer)
 
     # Activate the server; this will keep running until you
